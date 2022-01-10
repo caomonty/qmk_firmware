86c7421ff0e40634049222fdfeeb6c4c6287431a
Omar <omar@ixulabs.com>
2022-01-10 00:08:45 -0600

    added caps on utils layer

--- keyboards/preonic/keymaps/caomonty/keymap.c
+++ keyboards/preonic/keymaps/caomonty/keymap.c
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_PLUS, KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_RBRC, KC_BSLS, PIPE,    KC_EQL,
   _______, KC_F6,  KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_PGUP, MM_LE,   KC_UP,   MM_RI,   A(KC_E), KC_DEL,
   _______, DESK,   WMANA,   _______,  KC_F11,  KC_F12,  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_SCLN, _______,
-  _______, _______, _______, _______, _______, _______, _______, SP_LE,   _______, SP_RI,   BSLS,    _______,
+  KC_CAPS, _______, _______, _______, _______, _______, _______, SP_LE,   _______, SP_RI,   BSLS,    _______,
   _______, _______, _______, _______, CGUI,    SPOT,    SPOT,    _______, _______, _______, _______, _______
 ),
 /* Dektop Control
